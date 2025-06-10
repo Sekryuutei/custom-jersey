42b6a06c58146ad205aa4265528d677222de0533
Sekryuutei <anggi.craft99@gmail.com>
2025-06-11 01:02:55 +0700

    alpha24

--- resources/views/tutor.blade.php
+++ resources/views/tutor.blade.php
@@ -1,7 +1,7 @@
 @extends ('master')
 @section('content')
 <div>
-    <h2 class="display-5 fw-bolder justify-center"><span class="text-gradient d-inline">Langkah Membuat Jersey</span></h2>
+    <h2 class="display-5 fw-bolder text-center"><span class="text-gradient d-inline">Langkah Membuat Jersey</span></h2>
     <p>Pertama Tekan Tombol Buat Jersey</p>
     <img src="{{ asset('assets/step1.png') }}" alt="Step 1" style="width: 100px; height: auto;"><br>
     <p>Kedua Pilih Jersey</p>
@@ -10,7 +10,13 @@
     <img src="{{ asset('assets/step3.png') }}" alt="Step 3" style="width: 100px; height: auto;"><br>
     <p>Keempat Beli Jersey</p>
     <img src="{{ asset('assets/step4.png') }}" alt="Step 4" style="width: 100px; height: auto;"><br>
-    <p>Terakhir Cek Pesanan</p>
+    <p>Kelima Isi identitas</p>
     <img src="{{ asset('assets/step5.png') }}" alt="Step 5" style="width: 100px; height: auto;"><br>
+    <p>Keenam Bayar sesuai metode yang diinginkan</p>
+    <img src="{{ asset('assets/step6.png') }}" alt="Step 6" style="width: 100px; height: auto;"><br>
+    <p>Ketujuh Pembayaran Berhasil</p>
+    <img src="{{ asset('assets/step7.png') }}" alt="Step 7" style="width: 100px; height: auto;"><br>
+    <p>Terakhir Terima Kasih dan Mohon Ditunggu Pesanan Anda Sedang Diproses</p>
+    <img src="{{ asset('assets/step8.png') }}" alt="Step 8" style="width: 100px; height: auto;"><br>
 </div>
 @endsection
