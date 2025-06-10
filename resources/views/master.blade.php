8ebc2540141e5fae1f89a56178039fd45c3d30cc
Sekryuutei <anggi.craft99@gmail.com>
2025-06-10 20:48:16 +0700

    Alpha v6

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -27,7 +27,7 @@
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
-                            <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/">Cara Beli</a></li>
                         </ul>
                     </div>
                 </div>
@@ -35,7 +35,7 @@
             <!-- Header-->
             @yield('content')
             <!-- About Section-->
-            <section class="bg-light py-5">
+            <section class="bg-light py-4 mt-auto">
                 <div class="container px-5">
                     <div class="row gx-5 justify-content-center">
                         <div class="col-xxl-8">
@@ -43,13 +43,13 @@
                                 <!-- <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2> -->
                                 <ul class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></ul>
                                 <ll>
-                                    <a href="https://www.facebook.com/anggiez99" class="text-decoration-none text-dark"><img src="assets/facebook.png" width="150" height="150"><span class="text-gradient d-inline">Facebook</span></a>
+                                    <a href="https://www.facebook.com/anggiez99" class="text-decoration-none text-dark"><img src="assets/facebook.png" width="150" height="150"></a>
                                 </ll>
                                 <ll>
-                                    <a href="https://www.instagram.com/anggiez9" class="text-decoration-none text-dark"><img src="assets/instagram.png" width="150" height="150"><span class="text-gradient d-inline">Instagram</span></a>
+                                    <a href="https://www.instagram.com/anggiez9" class="text-decoration-none text-dark"><img src="assets/instagram.png" width="150" height="150"></a>
                                 </ll>
                                 <ll>
-                                    <a href="https://www.linkedin.com/in/anggie-abdurochman" class="text-decoration-none text-dark"><img src="assets/linkedin.png" width="150" height="150"><span class="text-gradient d-inline">Linkedin</span></a>
+                                    <a href="https://www.linkedin.com/in/anggie-abdurochman" class="text-decoration-none text-dark"><img src="assets/linkedin.png" width="150" height="150"></a>
                                 </ll>
                             </div>
                         </div>
